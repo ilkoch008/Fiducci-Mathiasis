438b0557a6a7ba8827e0d3382a06933e4abc8e8d
ilkoch008 <ilkoch008@gmail.com>
2021-04-10 11:10:59 +0300

    readme

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -53,41 +53,65 @@ public class App
         }
         graph.readFrom(input_file);
 
-//        File output = new File("balance_modes_log1.txt");
-//        try {
-//            if(output.createNewFile()){
-//                System.out.println("Output file created");
-//            } else {
-//                System.out.println("Overwriting existing file");
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//
-//        FileWriter output_writer;
-//        long start, end;
-//        try {
-//            output_writer = new FileWriter("balance_modes_log1.txt");
-//            for(int i = 2; i <= 4; i++){
-//                graph.score_mode = i;
-//                output_writer.write("score mode " + i + " ====================================\n");
-//                System.out.print("score mode " + i + " ====================================\n");
-//                for (int j = 0; j < 100; j++){
-//                    graph.random_partition();
-//                    graph.get_partition_score();
-//                    output_writer.write(graph.num_of_cuts + "; " + graph.lefts + "; " + graph.rights + "; ");
-//                    System.out.print(graph.num_of_cuts + "; " + graph.lefts + "; " + graph.rights + "; ");
-//                    start = System.currentTimeMillis();
-//                    graph.FM_with_gain_containers();
-//                    end = System.currentTimeMillis();
-//                    output_writer.write(graph.num_of_cuts + "; " + graph.lefts + "; " + graph.rights + "; " + (end-start) + "\n");
-//                    System.out.print(graph.num_of_cuts + "; " + graph.lefts + "; " + graph.rights + "; " + (end-start) + "\n");
-//                }
-//            }
-//            output_writer.close();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
+        File output = new File("balance_modes_log2.txt");
+        try {
+            if(output.createNewFile()){
+                System.out.println("Output file created");
+            } else {
+                System.out.println("Overwriting existing file");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        FileWriter output_writer;
+        long start, end;
+        try {
+            output_writer = new FileWriter("balance_modes_log2.txt");
+            for(int i = 0; i <= 3; i++){
+                switch (i){
+                    case 0:
+                        graph.take_from_end = false;
+                        graph.add_to_beginning = false;
+                        output_writer.write("default mode ====================================\n");
+                        System.out.print("default mode ====================================\n");
+                        break;
+                    case 1:
+                        graph.take_from_end = true;
+                        graph.add_to_beginning = false;
+                        output_writer.write("mode -tfe ====================================\n");
+                        System.out.print("mode -tfe ====================================\n");
+                        break;
+                    case 2:
+                        graph.take_from_end = false;
+                        graph.add_to_beginning = true;
+                        output_writer.write("mode -atb ====================================\n");
+                        System.out.print("mode -atb ====================================\n");
+                        break;
+                    case 3:
+                        graph.take_from_end = true;
+                        graph.add_to_beginning = true;
+                        output_writer.write("mode -tfe -atb ====================================\n");
+                        System.out.print("mode -tfe -atb ====================================\n");
+                        break;
+                }
+
+                for (int j = 0; j < 100; j++){
+                    graph.random_partition();
+                    graph.get_partition_score();
+                    output_writer.write(graph.num_of_cuts + "; " + graph.lefts + "; " + graph.rights + "; ");
+                    System.out.print(graph.num_of_cuts + "; " + graph.lefts + "; " + graph.rights + "; ");
+                    start = System.currentTimeMillis();
+                    graph.FM_with_gain_containers();
+                    end = System.currentTimeMillis();
+                    output_writer.write(graph.num_of_cuts + "; " + graph.lefts + "; " + graph.rights + "; " + (end-start) + "\n");
+                    System.out.print(graph.num_of_cuts + "; " + graph.lefts + "; " + graph.rights + "; " + (end-start) + "\n");
+                }
+            }
+            output_writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         graph.random_partition();
         graph.print_partition_info();
